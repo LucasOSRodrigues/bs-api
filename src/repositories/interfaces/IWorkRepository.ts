4290f0a00895320617547b8cf2b1f23be8c18d5b
Lucas O. S. Rodrigues <lucasosrodrigues13@gmail.com>
2026-05-22 16:38:52 -0300

    feat(crud-work): create work repository

---
+++ src/repositories/interfaces/IWorkRepository.ts
@@ -0,0 +1,9 @@
+import { Work } from "../../generated/prisma"
+
+export interface IWorkRepository {
+  createWork(work: Work, genres: string[]): Promise<Work>
+  getWorkById(work_id: string): Promise<Work | null>
+  updateWork(work_id: string, work: Partial<Work>): Promise<Work>
+  deleteWork(work_id: string): Promise<Work>
+  getWorksByGenre(genre: string): Promise<Work[]>
+}
---
+++ src/repositories/work.repository.ts
@@ -0,0 +1,39 @@
+import { prisma } from "../shared/prisma"
+import { Work } from "../generated/prisma"
+import { IWorkRepository } from "./interfaces/IWorkRepository"
+
+export class WorkRepository implements IWorkRepository {
+  async createWork(work: Work, genres: string[]): Promise<Work> {
+    return await prisma.work.create({
+      data: {
+        ...work,
+        genres: { connect: genres.map((genre_name) => ({ name: genre_name })) },
+      },
+    })
+  }
+
+  async getWorkById(work_id: string): Promise<Work | null> {
+    return await prisma.work.findUnique({ where: { work_id } })
+  }
+
+  async updateWork(work_id: string, work: Partial<Work>): Promise<Work> {
+    return await prisma.work.update({ where: { work_id }, data: work })
+  }
+
+  async deleteWork(work_id: string): Promise<Work> {
+    const work = await prisma.work.delete({ where: { work_id } })
+    return work
+  }
+
+  public async getWorksByGenre(genre: string): Promise<Work[]> {
+    return await prisma.work.findMany({
+      where: {
+        genres: {
+          some: {
+            name: genre
+          }
+        }
+      }
+    })
+  }
+}
