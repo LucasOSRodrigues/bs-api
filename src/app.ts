f648f0852a6c99bc631156078658fa5c9f0f5c3f
Lucas O. S. Rodrigues <lucasosrodrigues13@gmail.com>
2026-05-22 16:42:16 -0300

    feat(app.ts): add route '/work'

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import workRouter from "./routes/work.routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/works", workRouter);
+app.use(errorHandler)
 
 export default app;
