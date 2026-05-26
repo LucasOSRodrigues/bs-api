c6c9e0a1bde6a3290444e5ea2eb735ebd72a4e1d
Lucas O. S. Rodrigues <lucasosrodrigues13@gmail.com>
2026-05-26 15:02:03 -0300

    feat(work-model): realiza migration para tirar genre do work

---
+++ prisma/migrations/20260524191315_remove_genre_id_field_from_work_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `genre_id` on the `works` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "works" DROP COLUMN "genre_id";
