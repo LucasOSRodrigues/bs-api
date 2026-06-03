fadef88e3ce43e5034dc575241d725467de459ce
LucasOSRodrigues <lucasosrodrigues13@gmail.com>
2026-06-03 14:26:58 -0300

    fix(work type): remove genre_id field

--- src/shared/types/Work.ts
+++ src/shared/types/Work.ts
@@ -2,13 +2,12 @@ export type Work = {
     work_id: string
     title: string
     author_id: string
-    genre_id: string
     description: string | null
     cover_image_url: string | null
     published_date: Date | null
 }
 
-export interface WorkEntity extends Work { 
+export interface WorkEntity extends Work {
     created_at: Date
     updated_at: Date
 }
